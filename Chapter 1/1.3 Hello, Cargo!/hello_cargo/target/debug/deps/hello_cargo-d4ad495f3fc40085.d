25d81c4b88a548303acce823c4c2d77059dfbefc
ashiven <rodgiven@outlook.de>
2023-08-02 16:39:10 +0200

    changed folder structure

---
+++ Chapter 1/1.3 Hello, Cargo!/hello_cargo/target/debug/deps/hello_cargo-d4ad495f3fc40085.d
@@ -0,0 +1,8 @@
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\Rust-Practice\Chapter 1.3\hello_cargo\target\debug\deps\hello_cargo-d4ad495f3fc40085.rmeta: src\main.rs Cargo.toml
+
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\Rust-Practice\Chapter 1.3\hello_cargo\target\debug\deps\hello_cargo-d4ad495f3fc40085.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
