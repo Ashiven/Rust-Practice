38973ca168c0627e21890843358eb969a136d780
ashiven <nevisha@pm.me>
2023-08-05 14:57:42 +0200

    Refactor Config::new to use an iterator instead of a slice

--- Chapter 12/minigrep/src/lib.rs
+++ Chapter 12/minigrep/src/lib.rs
@@ -13,13 +13,18 @@ pub struct Config {
 // create separate function for parsing arguments
 // we have changed this into a constructor for Config to make things more idiomatic
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        // return an Err variant if not enough args
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn new(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         // if the environment variable IGNORE_CASE is set, we set the ignore_case field of the Config struct to true
         // the result of the env::var function is a Result enum which is of variant Ok if the env variable has been set to any value
@@ -58,29 +63,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 // the data returned by this function will live as long as the data passed in the contents parameter
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    // lines() returns an iterator over the lines inside of contents
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase(); // query becomes a String here so we have to pass &query to contains() later on
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
--- Chapter 12/minigrep/src/main.rs
+++ Chapter 12/minigrep/src/main.rs
@@ -4,13 +4,13 @@ use minigrep::Config;
 fn main() {
     // getting the user supplied arguments
     // using collect to turn the iterator into a vector
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
 
     // saving the relevant args inside of variables
     // some error handling if the constructor returns an Err variant
     // we print the contents of the error and explicitly exit the program with return code 1
     // unwrap_or_else only activates if unwrap encounters an Err variant and then executes the given code, passing the contents of Err to it
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
