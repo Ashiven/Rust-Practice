5296010be5b25ad41da27518d7a2d6dd87c5f832
ashiven <nevisha@pm.me>
2023-09-06 12:47:22 +0200

    change folder structure

---
+++ book/Chapter 19/19.2 Advanced Traits/advanced_traits/target/debug/deps/advanced_traits.d
@@ -0,0 +1,5 @@
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\rust-practice\Chapter 19\19.2 Advanced Traits\advanced_traits\target\debug\deps\advanced_traits.exe: src\main.rs
+
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\rust-practice\Chapter 19\19.2 Advanced Traits\advanced_traits\target\debug\deps\advanced_traits.d: src\main.rs
+
+src\main.rs:
---
+++ book/Chapter 19/19.3 Advanced Types/advanced_types/target/debug/deps/advanced_types.d
@@ -0,0 +1,5 @@
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\rust-practice\Chapter 19\19.3 Advanced Types\advanced_types\target\debug\deps\advanced_types.exe: src\main.rs
+
+C:\Users\janni\OneDrive\Dokumente\Projects\Rust\rust-practice\Chapter 19\19.3 Advanced Types\advanced_types\target\debug\deps\advanced_types.d: src\main.rs
+
+src\main.rs:
